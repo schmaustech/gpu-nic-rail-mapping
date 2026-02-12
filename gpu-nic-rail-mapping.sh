60c54a1ccf96fa7f2367f5fa31f725fe7fcc4cd0
Ben Schmaus <schmaustech@yahoo.com>
2026-02-12 14:52:53 -0600

    Update gpu-nic-rail-mapping.sh

--- gpu-nic-rail-mapping.sh
+++ gpu-nic-rail-mapping.sh
@@ -8,10 +8,18 @@ mapfile -t my_nics < <(lspci -nn|grep $nicid)
 
 for (( gpu=0; gpu<${#my_gpus[@]}; gpu++ ))
 do
-    echo ${my_gpus[$gpu]}
-done
+    gpubusid=`echo ${my_gpus[$gpu]} | awk '{print $1}'` 
+    gpupcisw=`lspci -d $gpuid -PP | grep $gpubusid | awk -F '/' {'print $1"/"$2'}`
+    #echo "GPU $gpupcisw $gpubusid"
+    for (( nic=0; nic<${#my_nics[@]}; nic++ ))
+    do
+       nicbusid=`echo ${my_nics[$nic]} | awk '{print $1}'`
+       nicpcisw=`lspci -d $nicid -PP | grep $nicbusid | awk -F '/' {'print $1"/"$2'}`
+       #echo "NIC $nicpcisw $nicbusid"
+       if [ "$nicpcisw" = "$gpupcisw" ]; then
+           nicslot=`dmidecode -t slot | grep -B4 $nicbusid|grep ID|awk -F ': ' {'print $2'}`
+           echo "GPU Bus Address: $gpubusid NIC Bus Address: $nicbusid Common PCIe Switch: $nicpcisw NIC SLOT: $nicslot"
+       fi
+    done
 
-for (( nic=0; nic<${#my_nics[@]}; nic++ ))
-do
-    echo ${my_nics[$nic]}
 done
