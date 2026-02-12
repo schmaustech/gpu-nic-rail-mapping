c6900099e884bd31c4eace3696aef52f7209ec12
Ben Schmaus <schmaustech@yahoo.com>
2026-02-12 15:29:32 -0600

    Update gpu-nic-rail-mapping.sh

--- gpu-nic-rail-mapping.sh
+++ gpu-nic-rail-mapping.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+##############################################################################################################
+# This script maps GPU/NIC/SLOT for ensuring only one Spectrum-X rail is assigned per GPU on same PCI Switch #
+##############################################################################################################
 
+### Need to enable passing of options from cli so we can specify our gpu device id and nic device id 
 gpuid="10de:2335"
 nicid="15b3:a2dc"
 
+# Slurp in the GPU devices 
 mapfile -t my_gpus < <(lspci -nn|grep $gpuid)
+# Slurp in the NIC devices
 mapfile -t my_nics < <(lspci -nn|grep $nicid)
 
+# This is where it gets real
 for (( gpu=0; gpu<${#my_gpus[@]}; gpu++ ))
 do
     gpubusid=`echo ${my_gpus[$gpu]} | awk '{print $1}'` 
@@ -17,9 +24,15 @@ do
        nicpcisw=`lspci -d $nicid -PP | grep $nicbusid | awk -F '/' {'print $1"/"$2'}`
        #echo "NIC $nicpcisw $nicbusid"
        if [ "$nicpcisw" = "$gpupcisw" ]; then
-           nicslot=`dmidecode -t slot | grep -B4 $nicbusid|grep ID|awk -F ': ' {'print $2'}`
-           echo "GPU Bus Address: $gpubusid NIC Bus Address: $nicbusid Common PCIe Switch: $nicpcisw NIC SLOT: $nicslot"
+           if [[ "$nicbusid" == *.1 ]]; then
+               nicport=2
+               altnicbusid=`echo $nicbusid | sed 's/.$/0/'`
+               nicslot=`dmidecode -t slot | grep -B4 $altnicbusid|grep ID|awk -F ': ' {'print $2'}`
+           else
+               nicport=1
+               nicslot=`dmidecode -t slot | grep -B4 $nicbusid|grep ID|awk -F ': ' {'print $2'}`
+           fi
+           echo "GPU Bus Address: $gpubusid NIC Bus Address: $nicbusid Common PCIe Switch: $nicpcisw NIC Slot: $nicslot NIC Port: $nicport UDEV Name:"
        fi
     done
-
 done
